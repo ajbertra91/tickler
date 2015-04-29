ac59a684b8edcf762e42cc5337da3f6b79f42719
ben.bertrand <bertrand.design@gmail.com>
2015-04-28 19:26:55 -0600

    Adds first passing test

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function Rolodex() {
+	var self = this;
+
+	// Calendar is a store of dates.
+	self.calendar = {};
+
+	return self;
+}
--- test/tickler.js
+++ test/tickler.js
@@ -1,6 +1,6 @@
 describe("ticklerApp", function () {
   it("has a calendar", function() {
-    var app = new Calendar();
-    expect(person.greet).toBe("Hello!");
+    var app = new Rolodex();
+    expect(app.calendar).toBeDefined();
   });
 });
